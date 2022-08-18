217e59569f1191ae4ceedc0992bfa1b2fd5032b7
Mitya Selivanov <0x7fffff@guattari.ru>
2022-08-18 21:07:53 +0400

    Write program headers.

--- min_back.c
+++ min_back.c
@@ -5,6 +5,11 @@
 #include <string.h>
 
 enum {
+  TARGET_386 = 1,
+  TARGET_X86_64,
+
+  ALIGNMENT_X86_64 = 16 * 1024,
+
   ELF32_HEADER_SIZE            = 52,
   ELF32_PROGRAM_HEADER_SIZE    = 32,
   ELF32_SECTION_HEADER_SIZE    = 40,
@@ -25,33 +30,59 @@ enum {
   ELF_PROGRAM_HEADER_XNUM      = 0xffff,
   ELF_SECTION_HEADER_LORESERVE = 0xff00,
   ELF_SECTION_HEADER_UNDEF     = 0,
-  ELF_SECTION_HEADER_XINDEX    = 0xffff
+  ELF_SECTION_HEADER_XINDEX    = 0xffff,
+  ELF_PROGRAM_HEADER_TYPE_LOAD = 1,
+  ELF_PROGRAM_HEADER_FLAG_R    = 4,
+  ELF_PROGRAM_HEADER_FLAG_W    = 2,
+  ELF_PROGRAM_HEADER_FLAG_X    = 1
 };
 
 typedef struct {
   uint8_t  *data;
   ptrdiff_t size;
+  ptrdiff_t offset;
+  ptrdiff_t address;
 } segment_t;
+
+typedef struct {
+  ptrdiff_t target;
+  ptrdiff_t alignment;
+  ptrdiff_t entry_point;
+
+  segment_t code;
+  segment_t text;
+  segment_t constant;
+  segment_t mutable;
+} program_t;
 
 typedef struct {
   uint8_t  architecture;
   uint16_t machine;
-  uint32_t machine_flags;
+  uint64_t entry_point;
+  uint32_t flags;
 
-  ptrdiff_t entry_point;
   ptrdiff_t program_header_table_offset;
   ptrdiff_t section_header_table_offset;
 
-  uint16_t program_header_table_entry_size;
-  uint16_t section_header_table_entry_size;
+  ptrdiff_t program_header_table_entry_size;
+  ptrdiff_t section_header_table_entry_size;
 
   ptrdiff_t program_header_table_size;
   ptrdiff_t section_header_table_size;
   ptrdiff_t section_name_string_table_index;
+} elf_t;
 
-  segment_t text;
-  segment_t data;
-} program_t;
+typedef struct {
+  uint8_t   architecture;
+  uint32_t  type;
+  uint32_t  flags;
+  ptrdiff_t offset;
+  ptrdiff_t virtual_address;
+  ptrdiff_t physical_address;
+  uint64_t  file_size;
+  uint64_t  memory_size;
+  uint64_t  alignment;
+} elf_program_header_t;
 
 typedef struct {
   int         status;
@@ -60,6 +91,12 @@ typedef struct {
   FILE       *f;
   ptrdiff_t   offset;
 } out_t;
+
+ptrdiff_t adjust_alignment(ptrdiff_t const offset,
+                           ptrdiff_t const alignment) {
+  ptrdiff_t const delta = offset % alignment;
+  return delta == 0 ? offset : offset + alignment - delta;
+}
 
 out_t parse_command_line(int argc, char **argv) {
   out_t out = { .status = 0,
@@ -125,15 +162,33 @@ out_t write_str(out_t out, char const *const s) {
   return write_bytes(out, (ptrdiff_t) strlen(s), s);
 }
 
-out_t write_byte(out_t out, uint8_t value) {
+out_t write_byte(out_t out, uint64_t value) {
+  if ((value & 0xffull) != value) {
+    out.status = -1;
+    out.error  = "Invalid u8 write.";
+    return out;
+  }
+
   return write_bytes(out, 1, &value);
 }
 
-out_t write_int16(out_t out, int16_t value) {
+out_t write_int16(out_t out, int64_t value) {
+  if ((value & 0xffffll) != value) {
+    out.status = -1;
+    out.error  = "Invalid i16 write.";
+    return out;
+  }
+
   return write_bytes(out, 2, &value);
 }
 
-out_t write_int32(out_t out, int32_t value) {
+out_t write_int32(out_t out, int64_t value) {
+  if ((value & 0xffffffffll) != value) {
+    out.status = -1;
+    out.error  = "Invalid i32 write.";
+    return out;
+  }
+
   return write_bytes(out, 4, &value);
 }
 
@@ -142,10 +197,22 @@ out_t write_int64(out_t out, int64_t value) {
 }
 
 out_t write_uint16(out_t out, uint16_t value) {
+  if ((value & 0xffffull) != value) {
+    out.status = -1;
+    out.error  = "Invalid u16 write.";
+    return out;
+  }
+
   return write_bytes(out, 2, &value);
 }
 
 out_t write_uint32(out_t out, uint32_t value) {
+  if ((value & 0xffffffffull) != value) {
+    out.status = -1;
+    out.error  = "Invalid u32 write.";
+    return out;
+  }
+
   return write_bytes(out, 4, &value);
 }
 
@@ -154,62 +221,50 @@ out_t write_uint64(out_t out, uint64_t value) {
 }
 
 program_t create_program() {
-  program_t program = { .architecture  = ELF_CLASS_ARCHITECTURE_64,
-                        .machine       = ELF_MACHINE_X86_64,
-                        .machine_flags = 0,
+  program_t program;
+  memset(&program, 0, sizeof program);
 
-                        .entry_point                 = 0,
-                        .program_header_table_offset = 0,
-                        .section_header_table_offset = 0,
-
-                        .program_header_table_entry_size = 0,
-                        .section_header_table_entry_size = 0,
-
-                        .program_header_table_size       = 0,
-                        .section_header_table_size       = 0,
-                        .section_name_string_table_index = 0 };
+  program.target = TARGET_X86_64;
 
   return program;
 }
 
-out_t write_elf_header(out_t out, program_t program) {
+out_t write_elf_header(out_t out, elf_t elf) {
   if (out.status != 0)
     return out;
 
-  if (program.architecture != ELF_CLASS_ARCHITECTURE_32 &&
-      program.architecture != ELF_CLASS_ARCHITECTURE_64) {
+  if (elf.architecture != ELF_CLASS_ARCHITECTURE_32 &&
+      elf.architecture != ELF_CLASS_ARCHITECTURE_64) {
     out.status = -1;
     out.error  = "Unsupported architecture.";
     return out;
   }
 
-  uint16_t elf_header_size = program.architecture ==
-                                     ELF_CLASS_ARCHITECTURE_32
-                                 ? ELF32_HEADER_SIZE
-                                 : ELF64_HEADER_SIZE;
+  uint16_t header_size = elf.architecture == ELF_CLASS_ARCHITECTURE_32
+                             ? ELF32_HEADER_SIZE
+                             : ELF64_HEADER_SIZE;
 
   uint16_t program_header_table_size_x16 = ELF_PROGRAM_HEADER_XNUM;
   uint16_t section_header_table_size_x16 = 0;
   uint16_t section_name_string_table_index_x16 =
       ELF_SECTION_HEADER_XINDEX;
 
-  if (program.program_header_table_size < ELF_PROGRAM_HEADER_XNUM)
-    program_header_table_size_x16 =
-        (uint16_t) program.program_header_table_size;
+  if (elf.program_header_table_size < ELF_PROGRAM_HEADER_XNUM)
+    program_header_table_size_x16 = (uint16_t)
+                                        elf.program_header_table_size;
 
-  if (program.section_header_table_size <
-      ELF_SECTION_HEADER_LORESERVE)
-    section_header_table_size_x16 =
-        (uint16_t) program.section_header_table_size;
+  if (elf.section_header_table_size < ELF_SECTION_HEADER_LORESERVE)
+    section_header_table_size_x16 = (uint16_t)
+                                        elf.section_header_table_size;
 
-  if (program.section_name_string_table_index <
+  if (elf.section_name_string_table_index <
       ELF_SECTION_HEADER_LORESERVE)
     section_name_string_table_index_x16 =
-        (uint16_t) program.section_name_string_table_index;
+        (uint16_t) elf.section_name_string_table_index;
 
   out = write_byte(out, ELF_MAGIC);
   out = write_str(out, "ELF");
-  out = write_byte(out, program.architecture);
+  out = write_byte(out, elf.architecture);
   out = write_byte(out, ELF_DATA_2_LITTLE_ENDIAN);
   out = write_byte(out, ELF_VERSION_CURRENT);
   out = write_byte(out, ELF_OS_ABI_UNIX_SYSTEM_V);
@@ -218,50 +273,112 @@ out_t write_elf_header(out_t out, program_t program) {
   out = write_byte(out, ELF_IDENT_SIZE);
 
   out = write_uint16(out, ELF_TYPE_EXECUTABLE);
-  out = write_uint16(out, program.machine);
+  out = write_uint16(out, elf.machine);
   out = write_uint32(out, ELF_VERSION_CURRENT);
 
-  if (program.architecture == ELF_CLASS_ARCHITECTURE_32) {
-    out = write_uint32(out, (uint32_t) program.entry_point);
-    out = write_uint32(
-        out, (uint32_t) program.program_header_table_offset);
-    out = write_uint32(
-        out, (uint32_t) program.section_header_table_offset);
+  if (elf.architecture == ELF_CLASS_ARCHITECTURE_32) {
+    out = write_uint32(out, elf.entry_point);
+    out = write_uint32(out, elf.program_header_table_offset);
+    out = write_uint32(out, elf.section_header_table_offset);
   } else {
-    out = write_uint64(out, (uint64_t) program.entry_point);
-    out = write_uint64(
-        out, (uint64_t) program.program_header_table_offset);
-    out = write_uint64(
-        out, (uint64_t) program.section_header_table_offset);
+    out = write_uint64(out, elf.entry_point);
+    out = write_uint64(out, elf.program_header_table_offset);
+    out = write_uint64(out, elf.section_header_table_offset);
   }
 
-  out = write_uint32(out, program.machine_flags);
-  out = write_uint16(out, elf_header_size);
+  out = write_uint32(out, elf.flags);
+  out = write_uint16(out, header_size);
 
-  out = write_uint16(out, program.program_header_table_entry_size);
+  out = write_uint16(out, elf.program_header_table_entry_size);
   out = write_uint16(out, program_header_table_size_x16);
-  out = write_uint16(out, program.section_header_table_entry_size);
+  out = write_uint16(out, elf.section_header_table_entry_size);
   out = write_uint16(out, section_header_table_size_x16);
   out = write_uint16(out, section_name_string_table_index_x16);
 
-  if (out.offset > elf_header_size) {
+  if (out.offset > header_size) {
     out.status = -1;
     out.error  = "Invalid ELF header offset.";
   } else
-    out = write_zero(out, elf_header_size - out.offset);
+    out = write_zero(out, header_size - out.offset);
 
   return out;
 }
 
-out_t write_program_headers(out_t out, program_t program) {
-  if (out.status != 0)
+out_t write_elf_program_header(out_t                out,
+                               elf_program_header_t header) {
+  out = write_uint32(out, header.type);
+
+  if (header.architecture == ELF_CLASS_ARCHITECTURE_32) {
+    out = write_uint32(out, header.offset);
+    out = write_uint32(out, header.virtual_address);
+    out = write_uint32(out, header.physical_address);
+    out = write_uint32(out, header.file_size);
+    out = write_uint32(out, header.memory_size);
+    out = write_uint32(out, header.flags);
+    out = write_uint32(out, header.alignment);
+  } else {
+    out = write_uint32(out, header.flags);
+    out = write_uint64(out, header.offset);
+    out = write_uint64(out, header.virtual_address);
+    out = write_uint64(out, header.physical_address);
+    out = write_uint64(out, header.file_size);
+    out = write_uint64(out, header.memory_size);
+    out = write_uint64(out, header.alignment);
+  }
+
+  return out;
+}
+
+out_t write_elf_load_header(out_t out, segment_t segment,
+                            uint8_t architecture, ptrdiff_t alignment,
+                            uint32_t flags) {
+  if (out.status != 0 || segment.size == 0)
     return out;
 
+  elf_program_header_t header = {
+    .architecture     = architecture,
+    .type             = ELF_PROGRAM_HEADER_TYPE_LOAD,
+    .flags            = flags,
+    .offset           = segment.offset,
+    .virtual_address  = segment.address,
+    .physical_address = segment.address,
+    .file_size        = segment.size,
+    .memory_size      = segment.size,
+    .alignment        = alignment
+  };
+
+  return write_elf_program_header(out, header);
+}
+
+out_t write_elf_program_headers(out_t out, program_t program,
+                                elf_t elf) {
+  if (out.status != 0 || elf.program_header_table_size == 0)
+    return out;
+
+  out = write_zero(out, elf.program_header_table_offset - out.offset);
+
+  out = write_elf_load_header(out, program.code, elf.architecture,
+                              program.alignment,
+                              ELF_PROGRAM_HEADER_FLAG_X);
+
+  out = write_elf_load_header(
+      out, program.text, elf.architecture, program.alignment,
+      ELF_PROGRAM_HEADER_FLAG_X | ELF_PROGRAM_HEADER_FLAG_R);
+
+  out = write_elf_load_header(out, program.constant, elf.architecture,
+                              program.alignment,
+                              ELF_PROGRAM_HEADER_FLAG_R);
+
+  out = write_elf_load_header(
+      out, program.mutable, elf.architecture, program.alignment,
+      ELF_PROGRAM_HEADER_FLAG_R | ELF_PROGRAM_HEADER_FLAG_W);
+
   return out;
 }
 
-out_t write_section_headers(out_t out, program_t program) {
-  if (out.status != 0)
+out_t write_elf_section_headers(out_t out, program_t program,
+                                elf_t elf) {
+  if (out.status != 0 || elf.section_header_table_size == 0)
     return out;
 
   return out;
@@ -271,23 +388,84 @@ out_t write_elf(out_t out, program_t program) {
   if (out.status != 0)
     return out;
 
-  if (program.architecture == ELF_CLASS_ARCHITECTURE_32) {
-    // program.program_header_table_offset = ELF32_HEADER_SIZE;
-    program.program_header_table_entry_size =
-        ELF32_PROGRAM_HEADER_SIZE;
-    program.section_header_table_entry_size =
-        ELF32_SECTION_HEADER_SIZE;
+  elf_t elf;
+  memset(&elf, 0, sizeof elf);
+
+  program.alignment = ALIGNMENT_X86_64;
+
+  if (program.target == TARGET_386) {
+    elf.architecture = ELF_CLASS_ARCHITECTURE_32;
+    elf.machine      = ELF_MACHINE_386;
+
+    elf.program_header_table_entry_size = ELF32_PROGRAM_HEADER_SIZE;
+    elf.section_header_table_entry_size = ELF32_SECTION_HEADER_SIZE;
+  } else if (program.target == TARGET_X86_64) {
+    elf.architecture = ELF_CLASS_ARCHITECTURE_64;
+    elf.machine      = ELF_MACHINE_X86_64;
+
+    elf.program_header_table_entry_size = ELF64_PROGRAM_HEADER_SIZE;
+    elf.section_header_table_entry_size = ELF64_SECTION_HEADER_SIZE;
   } else {
-    // program.program_header_table_offset = ELF64_HEADER_SIZE;
-    program.program_header_table_entry_size =
-        ELF64_PROGRAM_HEADER_SIZE;
-    program.section_header_table_entry_size =
-        ELF64_SECTION_HEADER_SIZE;
+    out.status = -1;
+    out.error  = "Unsupported target.";
+    return out;
   }
 
-  out = write_elf_header(out, program);
-  out = write_program_headers(out, program);
-  out = write_section_headers(out, program);
+  elf.entry_point = program.entry_point;
+
+  if (program.code.size > 0)
+    elf.program_header_table_size++;
+  if (program.text.size > 0)
+    elf.program_header_table_size++;
+  if (program.constant.size > 0)
+    elf.program_header_table_size++;
+  if (program.mutable.size > 0)
+    elf.program_header_table_size++;
+
+  ptrdiff_t const header_size = elf.architecture ==
+                                        ELF_CLASS_ARCHITECTURE_32
+                                    ? ELF32_HEADER_SIZE
+                                    : ELF64_HEADER_SIZE;
+
+  ptrdiff_t const headers_end = header_size +
+                                elf.program_header_table_entry_size *
+                                    elf.section_header_table_size +
+                                elf.section_header_table_entry_size *
+                                    elf.section_header_table_size;
+
+  if (elf.program_header_table_size != 0)
+    elf.program_header_table_offset = headers_end;
+
+  if (elf.section_header_table_size != 0)
+    elf.program_header_table_offset =
+        elf.program_header_table_offset == 0
+            ? headers_end
+            : elf.program_header_table_offset +
+                  elf.program_header_table_entry_size *
+                      elf.program_header_table_size;
+
+  program.code.offset = adjust_alignment(headers_end,
+                                         program.alignment);
+
+  program.text.offset = adjust_alignment(
+      program.code.offset + program.code.size, program.alignment);
+  program.text.address = adjust_alignment(program.code.size,
+                                          program.alignment);
+
+  program.constant.offset = adjust_alignment(
+      program.text.offset + program.text.size, program.alignment);
+  program.constant.address = adjust_alignment(program.text.size,
+                                              program.alignment);
+
+  program.mutable.offset  = adjust_alignment(program.constant.offset +
+                                                 program.constant.size,
+                                             program.alignment);
+  program.mutable.address = adjust_alignment(program.constant.size,
+                                             program.alignment);
+
+  out = write_elf_header(out, elf);
+  out = write_elf_program_headers(out, program, elf);
+  out = write_elf_section_headers(out, program, elf);
 
   return out;
 }
